7d85eb7f3fbc3ab9657e9973bedbbce6c773a8da
Martin Morgenstern <mail@mmorgenstern.net>
2018-02-15 18:42:43 +0100

    Add Scaladocs

--- benchmark/src/main/scala/scroll/benchmarks/bank/Transaction.scala
+++ benchmark/src/main/scala/scroll/benchmarks/bank/Transaction.scala
@@ -2,6 +2,11 @@ package scroll.benchmarks.bank
 
 import scroll.internal.Compartment
 
+/** Source and Target are intentionally *not* implemented as role types,
+  * to resemble the corresponding benchmark implementations of other
+  * role runtimes as closely as possible.
+  * Extending [[Compartment]] is however necessary to get the `+` operator.
+  */
 object Transaction extends Compartment {
   class Source(val account: Account) {
     def withdraw(amount: Float): Unit = {
